2b4aabbdcca290fd87ae1cc41e16479c6c304e9d
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-15 23:29:33 -0300

    estilizando os botões de status

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import * as S from './styles'
+
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag prioridade={prioridade}>{prioridade}</S.Tag>
+      <S.Tag status={status}>{status}</S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancearRemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancearRemover>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.BotaoCancearRemover>Remover</S.BotaoCancearRemover>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+export default Tarefa
