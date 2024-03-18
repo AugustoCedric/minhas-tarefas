cfcdfd9344662cf487d92985712cef4fe1dfbe9c
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-18 19:45:48 -0300

    configuração do checkbox e do comando edição

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import * as S from './styles'
 
 import TarefaClass from '../../models/Tarefa'
-import { editar, remover } from '../../store/reducers/tarefas'
+import { alteraStatus, editar, remover } from '../../store/reducers/tarefas'
 import { BotaoSalvar } from '../../styles'
+import * as enums from '../../utils/enums/Tarefas'
 
 type Props = TarefaClass
 
@@ -31,9 +32,29 @@ const Tarefa = ({
     setDescricao(descricaoOriginal)
   }
 
+  function alteraStatusTarefa(evento: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      alteraStatus({
+        id,
+        finalizado: evento.target.checked
+      })
+    )
+  }
+
   return (
     <S.Card>
-      <S.Titulo>{titulo}</S.Titulo>
+      <label htmlFor={titulo}>
+        <input
+          type="checkbox"
+          id={titulo}
+          checked={status === enums.Status.CONCLUIDA}
+          onChange={alteraStatusTarefa}
+        />
+        <S.Titulo>
+          {estaEditando && <em>Editando: </em>}
+          {titulo}
+        </S.Titulo>
+      </label>
       <S.Tag parametro="prioridade" prioridade={prioridade}>
         {prioridade}
       </S.Tag>
